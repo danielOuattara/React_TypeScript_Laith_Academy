f758e896ea6a7ab4977ec3889b59536de593e904
danielOuattara <daniel.ouattara@gmx.com>
2023-02-23 01:17:48 +0100

    logic update for props

--- 03_Props/src/App.tsx
+++ 03_Props/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import List from "./components/List";
+import { List1, List2, /* List3, */ List4, List5 } from "./components/List";
 
 interface PeopleState {
   people: {
@@ -18,7 +18,16 @@ function App() {
   return (
     <div className="App">
       <h1>People invited to our Party</h1>
-      <List people={people}/>
+      <hr />
+      <List1 people={people} number={number} title="React and TypeScript" />
+      <hr />
+      <List2 people={people} number={number} title="React and TypeScript" />
+      {/* <List3 people={people} number={number} title="React and TypeScript" /> */}
+      <hr />
+      <List4 people={people} number={number} title="React and TypeScript" />
+      <hr />
+      <List5 people={people} number={number} title="React and TypeScript" />
+      <hr />
     </div>
   );
 }
--- 03_Props/src/components/List.tsx
+++ 03_Props/src/components/List.tsx
@@ -5,17 +5,102 @@ interface PeopleProps {
     age: number;
     note?: string;
   }[];
+  number: number | string;
+  title: string;
 }
 
-// export default function List(props: PeopleProps) {
-//   return <div>List</div>;
-// }
+//-------------------------------------------------
 
+// OK
+export function List1(props: PeopleProps) {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <h3>The number is {props.number}</h3>
+      List
+      <ul>
+        {props.people.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+//-------------------------------------------------
+
+//OK
+export function List2({ number, people, title }: PeopleProps) {
+  return (
+    <>
+      <h2>{title}</h2>
+      <h3>The number is {number}</h3>
+      List:
+      <ul>
+        {people.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+//-------------------------------------------------
+
+// // ??
+// export function List3({
+//   number,
+//   people,
+//   title,
+// }: PeopleProps): React.FC<PeopleProps> {
+//   return (
+//     <>
+//       <h2>{title}</h2>
+//       <h3>The number is {number}</h3>
+//       List:
+//       <ul>
+//         {people.map((person) => (
+//           <li key={person.name}>{person.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
 
 // -----------------------------------------------
 
-const List: React.FC<PeopleProps> = ({ people }) => {
-  return <div>I am a list</div>;
+// OK, but general declaration:  return JSX.Element with props ...
+const List4 = (props: PeopleProps) => {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <h3>The number is {props.number}</h3>
+      List:
+      <ul>
+        {props.people.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
+export { List4 };
 
- export default List;
+//-------------------------------------------------
+
+// OK, and more specific: return React.Functional Component with props ...
+const List5: React.FC<PeopleProps> = ({ people, number, title }) => {
+  return (
+    <>
+      <h2>{title}</h2>
+      <h3>The number is {number}</h3>
+      List:
+      <ul>
+        {people.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export { List5 };
